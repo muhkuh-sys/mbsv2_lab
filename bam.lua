978cf8aa66b1d3ce2bf724aab90a31d5c3da0eae
Yule1988 <simontru1988@gmail.com>
2022-11-22 00:33:12 +0100

    Add BUILD_TYPE and Add BUILD_TYPE. Replace Template function by VersionTemplate. Add HBootImage builder to create binary file of netx90_com_intram.

--- bam.lua
+++ bam.lua
@@ -6,9 +6,7 @@
 -- Setup the Muhkuh build system.
 Import('mbs2/mbs.lua')
 
-
 local pl = require'pl.import_into'()
-
 
 ----------------------------------------------------------------------------------------------------------------------
 --
@@ -16,6 +14,18 @@ local pl = require'pl.import_into'()
 --
 
 local atEnv = _G.atEnv
+
+-- Set some variables.
+-- FIXME: Move this to build.properties and CLI.
+atEnv.DEFAULT.atVars.BUILD_TYPE = 'DEBUG'
+
+-- FIXME: Move this to setup.json file
+atEnv.DEFAULT.atVars.PROJECT_VERSION =
+{
+  [1] = "1",
+  [2] = "0",
+  [3] = "0",
+}
 
 atEnv.NETX500 = atEnv.DEFAULT:CreateEnvironment{'gcc-arm-none-eabi-4.7'}
   :AddCompiler('NETX500')
@@ -39,29 +49,24 @@ atEnv.NETX90 = atEnv.DEFAULT:CreateEnvironment{'gcc-arm-none-eabi-4.9'}
 --
 -- Build the platform library.
 --
-SubBAM('platform/bam.lua')
 
+SubBAM('platform/bam.lua')
 
 --------------------------------------------------------------------------------------------------------------
 --
 -- Filter the version.h file.
 --
-local tVersionFile = atEnv.DEFAULT:Template(
-  'targets/version/version.h',
-  'templates/version.h',
-  {
-    PROJECT_VERSION_MAJOR = '1',
-    PROJECT_VERSION_MINOR = '2',
-    PROJECT_VERSION_MICRO = '3',
-    PROJECT_VERSION_VCS = 'GITabc'
-  }
-)
 
+local tVersionFile = atEnv.DEFAULT:VersionTemplate(
+  'targets/version/version.h', -- output path
+  'templates/version.h' -- input path
+)
 
 --------------------------------------------------------------------------------------------------------------
 --
 -- Build blinki.
 --
+
 local atBlinkiEnvironments = {
   atEnv.NETX4000,
   atEnv.NETX500,
@@ -70,8 +75,38 @@ local atBlinkiEnvironments = {
   atEnv.NETX10,
   atEnv.NETX90
 }
+
+local atLdFiles = {
+  NETX4000 = 'src/netx4000/netx4000_cr7_intram.ld',
+  NETX90 = 'src/netx90/netx90_com_intram.ld',
+  NETX500 = 'src/netx500/netx500_intram.ld',
+  NETX50 = 'src/netx50/netx50_intram.ld',
+  NETX56 = 'src/netx56/netx56_intram.ld',
+  NETX10 = 'src/netx10/netx10_intram.ld'
+}
+
+local atTargetBinFiles = {
+  NETX4000 = 'targets/blinki_netx4000_com_intram.bin',
+  NETX90 = 'targets/blinki_netx90_com_intram.bin',
+  NETX500 = 'targets/blinki_netx500_com_intram.bin',
+  NETX50 = 'targets/blinki_netx50_com_intram.bin',
+  NETX56 = 'targets/blinki_netx56_com_intram.bin',
+  NETX10 = 'targets/blinki_netx10_com_intram.bin'
+}
+
+local astrBlinkiSources = {
+  'src/hboot_dpm.c',
+  'src/header.c',
+  'src/init.S',
+  'src/main.c'
+}
+
+local atEnvLink = {}
 for _, tBaseEnv in ipairs(atBlinkiEnvironments) do
   local tEnv = tBaseEnv:Clone()
+
+  -- Save the link object
+  atEnvLink[tEnv.atVars.COMPILER_ID] = tEnv
 
   -- Set include paths for the platform lib.
   tEnv:AddInclude{
@@ -81,12 +116,6 @@ for _, tBaseEnv in ipairs(atBlinkiEnvironments) do
     'targets/version'
   }
 
-  local astrBlinkiSources = {
-    'src/hboot_dpm.c',
-    'src/header.c',
-    'src/init.S',
-    'src/main.c'
-  }
   -- Set ouput path for all sources in "src" to "targets/netx90_com_intram".
   tEnv:SetBuildPath(
     'src',
@@ -95,19 +124,49 @@ for _, tBaseEnv in ipairs(atBlinkiEnvironments) do
   -- Build all sources.
   local atObjectsBlinki = tEnv:Compile(astrBlinkiSources)
 
-  local atLdFiles = {
-    NETX4000 = 'src/netx4000/netx4000_cr7_intram.ld',
-    NETX90 = 'src/netx90/netx90_com_intram.ld',
-    NETX500 = 'src/netx500/netx500_intram.ld',
-    NETX50 = 'src/netx50/netx50_intram.ld',
-    NETX56 = 'src/netx56/netx56_intram.ld',
-    NETX10 = 'src/netx10/netx10_intram.ld'
-  }
+  -- Add the extension of ELF file
+  tEnv:SetLinkExtension(".elf")
+
   -- Now link everything to an ELF file.
   local tElf = tEnv:Link(
-    string.format('targets/blinki_%s_intram.elf', string.lower(tEnv.atVars.COMPILER_ID)),
+    string.format('targets/%s_intram/blinki_%s_intram.elf', string.lower(tEnv.atVars.COMPILER_ID),string.lower(tEnv.atVars.COMPILER_ID)),
     atLdFiles[tEnv.atVars.COMPILER_ID],
     atObjectsBlinki,
     tEnv.atVars.PLATFORM_LIB
   )
+
+  -- Add file path of ELF file
+  tEnv.atVars["Path_ElfFile"] = tElf
+
+  -- Create a ObjCopy file.
+  local tObjCopy = tEnv:ObjCopy(
+    string.format('targets/%s_intram/blinki_%s_intram.bin', string.lower(tEnv.atVars.COMPILER_ID),string.lower(tEnv.atVars.COMPILER_ID)),
+    tElf
+  )
+
+  -- Add file path of ObjCopy file
+  tEnv.atVars["Path_ObjCopyFile"] = tObjCopy
+
+  -- Create a ObjDump file.
+  local tObjDump = tEnv:ObjDump(
+    string.format('targets/%s_intram/blinki_%s_intram.txt', string.lower(tEnv.atVars.COMPILER_ID),string.lower(tEnv.atVars.COMPILER_ID)),
+    tElf
+  )
+
+  -- Add file path of ObjDump file
+  tEnv.atVars["Path_ObjDumpFile"] = tObjDump
+
 end
+
+-- Create a binary file.
+atEnvLink["NETX90"]:HBootImage(
+  string.format('targets/blinki_%s_com_intram.bin', string.lower(atEnvLink["NETX90"].atVars.COMPILER_ID)),
+  string.format("src/%s/COM_to_INTRAM.xml",string.lower(atEnvLink["NETX90"].atVars.COMPILER_ID)),
+  atEnvLink["NETX90"].atVars["Path_ElfFile"],
+  {
+    objcopy = atEnvLink["NETX90"].atVars["OBJCOPY"],
+    objdump = atEnvLink["NETX90"].atVars["OBJDUMP"],
+    alias = {tElfCOM = atEnvLink["NETX90"].atVars["Path_ElfFile"]},
+    ["netx-type"] = atEnvLink["NETX90"].atVars.COMPILER_ID,
+  }
+  )
